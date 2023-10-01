5a321e274508ef01a3e4a494141dc0176345bd90
sabirnc <mohammedsabir5123@gmail.com>
2023-10-01 16:26:43 +0530

    added a contact page

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { Contact } from "../pages/Contact";
 
 
 export const router = createBrowserRouter([
     {
         path:"/",
         element:<Home />
+    },
+    {
+        path:"/contact",
+        element:<Contact />
     }
 ])
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,28 @@
+import { Navbar } from "../components/navbar";
+import "../css/contact.css";
+
+export const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="contact-sec">
+        <div>
+            <div className="contact-head-container">
+                <h3 className="contact-head">Contact us</h3>
+                <p className="contact-para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+                    odio eius a voluptatibus totam repellendus esse, mollitia quasi nemo
+                    deleniti minima, harum quo fugiat. Possimus, dolor laboriosam
+                    pariatur ex error commodi reprehenderit magni vero quam ipsam sit
+                    tempore sint inventore dolores aliquid voluptatum repudiandae eaque
+                    ad libero? Veritatis officia facere exercitationem doloremque culpa
+                    fuga, mollitia magnam dolor id similique dolorum ipsam, at
+                    perspiciatis voluptatibus necessitatibus ab amet numquam, maiores
+                    odit.
+                </p>
+            </div>
+        </div>
+      </section>
+    </>
+  );
+};
